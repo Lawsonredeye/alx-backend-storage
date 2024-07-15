9ffd94f95c6565a4fef1b9ffd4692a95287dc5dc
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-07-15 23:06:30 +0100

    pycodestyle fix

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -5,6 +5,13 @@ Interact with the Mongodb with the pymongo Lib
 
 
 def list_all(mongo_collection):
+    """
+    parameter:
+        mongo_collection: mongodb_object
+
+    return:
+        empty list if no document in the collection
+    """
     document_object = mongo_collection.find()
     if document_object:
         return list(document_object)
