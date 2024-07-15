af155a475d5903105af4e98c820ca9ab9b875a72
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-07-15 23:03:17 +0100

    python pymongo for handling mongo queries

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -2,3 +2,11 @@
 """
 Interact with the Mongodb with the pymongo Lib
 """
+
+
+def list_all(mongo_collection):
+    document_object = mongo_collection.find()
+    if document_object:
+        return list(document_object)
+    else:
+        return []
