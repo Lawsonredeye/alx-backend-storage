b0b45e8154b0fe20708f751cfb547c23f6597874
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-07-16 14:33:27 +0100

    function that returns the list of school having a specific topic

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -5,4 +5,22 @@ Python function that returns the list of school having a specific topic
 
 
 def schools_by_topic(mongo_collection, topic: str):
-    return ""
+    """
+    Parameters:
+        mongo_collection: object
+            mongo object which passes access to the mongo collection cursor
+        topic: str
+            filter to be passed into function for searching
+
+    Returns:
+        list
+            all document object that matches the filter
+    """
+    mongo_list = []
+    mongo_data = mongo_collection.find({"topics": topic})
+
+    # iterate through the data object recieved from the database
+    for data in mongo_data:
+        mongo_list.append(data)
+
+    return mongo_list
