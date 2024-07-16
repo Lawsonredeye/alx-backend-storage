491fe23c9f0d14a0ffddb2470d464437e6b99dd5
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-07-16 19:27:37 +0100

    (fix): changed the string format for patch and made the file executable

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,11 +3,6 @@
 Python script that provides some stats about Nginx logs stored in MongoDB:
 """
 
-# use pymongo library for getting access to the data
-# create a client connection to connect to the database and the collection
-# using the method .count() to get the total count
-# .count({filter})
-
 import pymongo
 
 client = pymongo.MongoClient()
@@ -24,8 +19,10 @@ mthd = {
 
 status_path = db.nginx.count({"path": "/status"})
 
+patch = f"\n\tmethod PUT: {mthd['PUT']}\n\tmethod PATCH: {mthd['PATCH']}"
+
 print(f"{total_logs} logs\nMethods:")
 print(f"\tmethod GET: {mthd['GET']}\n\tmethod POST: {mthd['POST']}", end="")
-print(f"\n\tmethod PUT: {mthd['PUT']}\n\tmethod PATCH: {mthd['PATCH']}", end="")
+print(patch, end="")
 print(f"\n\tmethod DELETE: {mthd['DELETE']}")
 print(f"{status_path} status check")
