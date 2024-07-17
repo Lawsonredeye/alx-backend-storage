12ac6281ab159ae0d2770d717e23280fb54a915c
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-07-17 23:46:02 +0100

    (fix): pycodestyle

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,6 +8,7 @@ import redis
 from uuid import uuid4
 import typing
 
+
 class Cache:
     """
     stores the data into hash on the redis server but flushes the db first
@@ -18,7 +19,7 @@ class Cache:
         """initilizer"""
         self._redis = redis.Redis()
         self._redis.flushdb()
-    
+
     def store(self, data: typing.Any) -> str:
         """
         stores the persisted data into the redis cache system with a uuid4
@@ -27,11 +28,11 @@ class Cache:
             data: typing.Any
                 data to be stored into the database which could ne a byte, str
                 integer, or even list
-        
+
         Returns:
             key: str
                 string value of what was passed to the cache
         """
-        key:str = str(uuid4())
+        key: str = str(uuid4())
         self._redis.set(key, data)
         return key
